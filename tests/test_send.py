538d0b90f40927f7abd66beb2547c3aa8bd9219c
Tibor Leupold <tibor@lpld.io>
2020-04-16 11:12:58 -0700

    Add test_send todo

--- tests/test_send.py
+++ tests/test_send.py
@@ -3,10 +3,12 @@
 
 # TODO: Test sending in test mode does not create a history entry.
 # TODO: Test sending in test mode shows a console message.
+# TODO: Test sending in test mode shows the console message even if tweet already in history.
 # TODO: Test sending in test mode does not trigger the tweepy API to send.
 # TODO: Test sending in test mode does not create a tweepy API instance.
 
 # TODO: Test sending in normal mode creates a history entry.
+# TODO: Test sending duplicate in normal mode leads to error to user.
 # TODO: Test sending in normal mode shows a console message.
 # TODO: Test sending in normal mode prints success msg to console
 # TODO: Test sending in normal mode triggers the tweepy API to send.
