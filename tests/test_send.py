eb624c872f24797e57df3fe56533a35b96eece90
Tibor Leupold <tibor@lpld.io>
2020-04-16 10:40:47 -0700

    Add todos for test_send

---
+++ tests/test_send.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+# TODO: Test sending in test mode does not create a history entry.
+# TODO: Test sending in test mode shows a console message.
+# TODO: Test sending in test mode does not trigger the tweepy API to send.
+# TODO: Test sending in test mode does not create a tweepy API instance.
+
+# TODO: Test sending in normal mode creates a history entry.
+# TODO: Test sending in normal mode shows a console message.
+# TODO: Test sending in normal mode prints success msg to console
+# TODO: Test sending in normal mode triggers the tweepy API to send.
+# TODO: Test issue to send shows error to user.
+# TODO: Test sending in normal mode creates an tweepy API instance.
+# TODO: Test issue to create a tweepy API instance shows error to user.
