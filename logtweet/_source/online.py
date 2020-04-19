fa4b11650ca751d620443121ccbd87fd57255eab
Tibor Leupold <tibor@lpld.io>
2020-04-19 16:23:03 -0700

    Move url validation to static method
    Done to keep the initialization function slim. Same functionality is
    still ensured.
    
    Additional steps in initialization might be to access the resource.
    Failing to access would result in another validation error, because the
    log source is practically unusable.

--- logtweet/_source/online.py
+++ logtweet/_source/online.py
@@ -2,6 +2,7 @@
 
 """Defines class representing a valid online source for the log."""
 
+from typing import Literal
 
 import validators  # type: ignore
 
@@ -18,6 +19,29 @@ class OnlineLogSource(object):
         During initialization, it is validated that the given ``source_string``
         is in fact a valid URL representation.
 
+        Arguments:
+            source_string (str): String that identifies the online resource
+                which should be validated.
+
+        Raises:
+            NotAUrlError: if the given ``source_string`` does not represent a
+                valid URL.
+
+        """
+        self.raise_for_invalid_url(source_string)
+        self.source_string = source_string
+
+    @staticmethod
+    def raise_for_invalid_url(source_string: str) -> None:
+        """
+        Raise an exception if the given source string is not a valid URL.
+
+        Arguments:
+            source_string (str): URL that is to be validated.
+
+        Returns:
+            True: if the given ``source_string`` is in fact a valid URL.
+
         Raises:
             NotAUrlError: if the given ``source_string`` does not represent a
                 valid URL.
@@ -26,4 +50,3 @@ class OnlineLogSource(object):
         is_url = validators.url(source_string)
         if is_url is not True:
             raise NotAUrlError(source_string)
-        self.source_string = source_string
--- tests/_source/test_online.py
+++ tests/_source/test_online.py
@@ -9,31 +9,49 @@ import pytest  # type: ignore
 # source.
 
 
-def test_valid_url_creates_instance():
-    from logtweet._source.online import OnlineLogSource
+class TestInitialization(object):
+    """Test the initialization of ``OnlineLogSource``."""
+    def test_valid_url_creates_instance(self):
+        from logtweet._source.online import OnlineLogSource
 
-    instance = OnlineLogSource("https://example.com")
+        instance = OnlineLogSource("https://example.com")
 
-    assert isinstance(instance, OnlineLogSource)
+        assert isinstance(instance, OnlineLogSource)
+
+    def test_invalid_url_raises_exception(self):
+        from logtweet._source.online import OnlineLogSource
+        from logtweet._source.exceptions import NotAUrlError
+
+        with pytest.raises(NotAUrlError):
+            OnlineLogSource("not a url, just a string")
 
 
-def test_invalid_url_raises_exception():
-    from logtweet._source.online import OnlineLogSource
-    from logtweet._source.exceptions import NotAUrlError
 
-    with pytest.raises(NotAUrlError):
-        OnlineLogSource("not a url, just a string")
+class TestRaiseForInvalidUrlStaticMethod(object):
+    """Test the static ``raise_for_invalid_url`` method."""
+    def test_valid_url_returns_none(self):
+        from logtweet._source.online import OnlineLogSource
 
+        is_valid = OnlineLogSource.raise_for_invalid_url("https://example.com")
 
-def test_invalid_url_shown_in_raised_exception():
-    from logtweet._source.online import OnlineLogSource
-    from logtweet._source.exceptions import NotAUrlError
-    source_string = "not a url, just a string"
+        assert is_valid is None
 
-    with pytest.raises(
-        NotAUrlError,
-        match=r"^The given source string '{0}' is not a URL!".format(
-            source_string,
-        ),
-    ):
-        OnlineLogSource(source_string)
+    def test_invalid_url_raises_exception(self):
+        from logtweet._source.online import OnlineLogSource
+        from logtweet._source.exceptions import NotAUrlError
+
+        with pytest.raises(NotAUrlError):
+            OnlineLogSource.raise_for_invalid_url("not a url, just a string")
+
+    def test_invalid_url_shown_in_raised_exception(self):
+        from logtweet._source.online import OnlineLogSource
+        from logtweet._source.exceptions import NotAUrlError
+        source_string = "not a url, just a string"
+
+        with pytest.raises(
+            NotAUrlError,
+            match=r"^The given source string '{0}' is not a URL!".format(
+                source_string,
+            ),
+        ):
+            OnlineLogSource.raise_for_invalid_url(source_string)
