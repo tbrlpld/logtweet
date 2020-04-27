eb59ad42f65781200772a7b5324526a395e6632f
Tibor Leupold <tibor@lpld.io>
2020-04-27 12:12:18 -0700

    Use Numpy docstring style.

--- logtweet/_source/online.py
+++ logtweet/_source/online.py
@@ -14,36 +14,50 @@ from logtweet._source.valid_url import ValidUrl  # noqa: WPS436
 class OnlineLogSource(object):
     """Valid online log source."""
 
-    def __init__(self, source_string: str):
+    def __init__(self, url: str):
         """
         Initialize OnlineLogSource object.
 
-        During initialization, it is validated that the given ``source_string``
+        During initialization, it is validated that the given `source_string`
         is in fact a valid URL representation.
 
         Also, the online source is only valid if the content can successfully
-        be retrieved. Thus, a successful instantiation makes the  ``content``
+        be retrieved. Thus, a successful instantiation makes the  `content`
         available in the same named property. If anything fails while
         retrieving the content, the instantiation fails and no element is
         created. The occurring exceptions are not caught and populate to the
         caller.
 
-        Arguments:
-            source_string (str): String that identifies the online resource
-                which should be validated.
+        Parameters
+        ----------
+        url : str
+            String that identifies the online resource which should
+            be validated.
+
+        Raises
+        ------
+        NotAUrlError:
+            If the passed URL is not a valid URL.
+        RequestError:
+            When the network connection to the URL target fails.
+        HTTPStatusError:
+            When the URL host responds with an error status code.
 
         """
-        self.url = ValidUrl(source_string)
+        self.url = ValidUrl(url)
 
         self._content = get_content_from_url(self.url)  # noqa: WPS110
 
-    @property
-    def content(self):  # noqa: WPS110
+    @property  # noqa: WPS110
+    def content(self):
         """
         Return online log content.
 
-        Returns:
-            str: content of online log source
+        Returns
+        -------
+        str
+            Content of online log source.
+
         """
         return self._content  # noqa: WPS110
 
@@ -52,17 +66,24 @@ def get_content_from_url(valid_url: ValidUrl) -> str:
     """
     Get content from online source.
 
-    Arguments:
-        valid_url (ValidUrl): Validated url object from which the content
-            should be retrieved.
+    Parameters
+    ----------
+    valid_url : ValidUrl
+         Validated url object from which the content should be retrieved.
 
-    Returns:
-        str: Content retrieved from the URL.
+    Returns
+    -------
+    str
+        Content retrieved from the URL.
 
-    Raises:
-        TypeError: when the passed URL is not of type ``ValidUrl``.
-        RequestError: when the network connection to the URL target fails.
-        HTTPStatusError: when the URL host responds with an error status code.
+    Raises
+    ------
+    TypeError
+        When the passed URL is not of type `ValidUrl`.
+    RequestError
+        When the network connection to the URL target fails.
+    HTTPStatusError
+        When the URL host responds with an error status code.
 
     """
     if not isinstance(valid_url, ValidUrl):
