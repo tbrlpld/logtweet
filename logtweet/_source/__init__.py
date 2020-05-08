812e46c2560eef7e08619eec81743382977399c5
Tibor Leupold <tibor@lpld.io>
2020-05-07 18:09:38 -0700

    Add TODOs

--- logtweet/_source/__init__.py
+++ logtweet/_source/__init__.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 
 """Private package with helpers modules for use by the ``source`` module."""
+
+# TODO: (1) Refactor to clean architecture where use case is not dependent on
+#       implementation
