6969768cfaba5f971ea753373dda725d4bd8d77f
Tibor Leupold <tibor@lpld.io>
2020-05-14 12:46:54 -1000

    Implement retrieve controller

---
+++ logtweet/source/controllers/retrieve.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""
+Controller for the log content retrieval.
+
+This is the entry point to the retrieve usecase.
+
+"""
+
+from logtweet.source.usecases import retrieve as ucretrieve
+from logtweet.source.adapters import onlineretriever as adaptonline
+from logtweet.source.adapters import validurl as adapturl
+
+
+def get_log_content_from_source(source_string: str) -> str:
+    """
+    Return log content from the source identified by the source string.
+
+    Parameters
+    ----------
+    source_string : str
+        String defining the source from which to retrieve the content.
+        Currently, the source string has to be a valid url. But, this will
+        be extended to allow local file paths in the future.
+
+    Returns
+    -------
+    str
+        Content string retrieved from the source.
+
+    """
+    validurl = adapturl.ValidSourceURL(source_string)
+    retriever = adaptonline.OnlineSourceContentRetriever(validurl)
+    return ucretrieve.get_log_content_from_source(retriever)
