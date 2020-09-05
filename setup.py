f25a2ad53196ce5df082567395ed73a4a6e725a7
Tibor Leupold <tibor@lpld.io>
2020-09-05 13:01:55 -1000

    Update project URL in setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     description="Create a tweet based on a #100DaysOfCode log message",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/tbrlpld/100daysofweb-with-python-course/tree/master/work/078-twitter-bot",
+    url="https://github.com/tbrlpld/logtweet",
     python_requires=">=3.6",
     install_requires=requires,
     extras_require={
